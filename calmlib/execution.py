d938dce5c17a2cb1e7f06d7a3a0b0a09f216543b
Calm Quant <calmquant@gmail.com>
2020-02-23 16:30:22 +0300

    add run_bg
    run_bg
      execute any python method in a background thread.
    
    add docstrings
    optimize imports

--- calmlib/execution.py
+++ calmlib/execution.py
@@ -1,7 +1,15 @@
 # 1) run external binary - basically any bash command
-import subprocess
+import threading
+from subprocess import Popen, PIPE
+
+
 def run_cmd(cmd, stdin=''):
-    from subprocess import Popen, PIPE
+    """
+    Execute any bash command. Return its output.
+    :param cmd:
+    :param stdin:
+    :return:
+    """
 
     p = Popen(cmd.split(), stdin=PIPE, stdout=PIPE, stderr=PIPE)
     output, err = p.communicate(stdin.encode())
@@ -9,5 +17,19 @@ def run_cmd(cmd, stdin=''):
     if rc:
         raise Exception(err)
     return output.decode()
+
+
 # 2) run python code - eval
 # 3) run python function in backgroud - threading, subprocess etc.
+
+def run_bg(func, *args, **kwargs):
+    """
+    Execute any callable in a background thread
+    :param func: target func for thread
+    :param args: args for func
+    :param kwargs: kwargs for func
+    :return: threading.Thread object
+    """
+    t = threading.Thread(target=func, args=args, kwargs=kwargs)
+    t.start()
+    return t
