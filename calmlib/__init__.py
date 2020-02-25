6630f0ec1ae3a54072c37f89cf764bfbe56cd19a
Calm Quant <calmquant@gmail.com>
2020-02-25 05:12:51 +0300

    Add trim and rtrim

--- calmlib/__init__.py
+++ calmlib/__init__.py
@@ -1,5 +1,5 @@
 from .execution import run_cmd, run_bg
-from .unsorted import fix_path
+from .unsorted import fix_path, trim, rtrim
 from .read_write import load, dump, load_json, dump_json, load_pickle, dump_pickle
 from .autocast import autocast_args
 from .logging_utils import get_personal_logger
--- calmlib/unsorted.py
+++ calmlib/unsorted.py
@@ -6,3 +6,15 @@ from calmlib.autocast import autocast_args
 @autocast_args
 def fix_path(path: Path):
     return path.expanduser().absolute()
+
+
+def trim(l: str, s="", e=""):
+    if l.startswith(s):
+        l = l[len(s) or None:]
+    if l.endswith(e):
+        l = l[:-len(e) or None]
+    return l
+
+
+def rtrim(l, e):
+    return trim(l, e=e)
